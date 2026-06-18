3c62fd65fe4216646b0a1e2618e6644c0610c146
Nodaco35 <ngodoancuong699668@gmail.com>
2026-06-18 10:19:02 +0700

    fix: hero section

--- config/api.ts
+++ config/api.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NODE_ENV === "development"
-  ? "http://localhost:3000"
-  : "https://exe-kindness-connector-be.onrender.com";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_PROD ||
+  "https://exe-kindness-connector-be.onrender.com";
